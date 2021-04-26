5361f9d35c0728f248d4f85ed6a858ed589c8f20
Tina Wang <mwow@yopmail.com>
2021-04-27 00:08:03 +0200

    🎨 Better text color contrast

--- layouts/post.vue
+++ layouts/post.vue
@@ -31,14 +31,14 @@
 }
 .post__container h4 {
   font-family: "Fira Sans";
-  @apply text-sm italic font-bold text-gray-500;
+  @apply text-sm italic font-bold text-gray-600;
 }
 .post__container h5 {
   @apply mb-6;
 }
 .post__container p {
   @apply mb-6;
-  @apply text-base font-light text-gray-500 tracking-wide;
+  @apply text-base font-light text-gray-600 tracking-wide;
 }
 
 .nuxt-content h1 {
@@ -67,9 +67,15 @@
 .nuxt-content blockquote p {
   @apply mb-0;
 }
-.nuxt-content pre {
+.nuxt-content .nuxt-content-highlight {
   @apply w-4/5;
-  @apply mb-6 mx-auto;
+  @apply mx-auto;
+}
+.nuxt-content .filename {
+  @apply text-sm text-gray-600 italic;
+}
+.nuxt-content pre {
+  @apply mt-0 mb-6 mx-auto;
   @apply rounded-lg bg-gray-50;
   @apply text-gray-500;
 }
@@ -78,13 +84,12 @@
   @apply rounded bg-gray-100 text-gray-700;
   /* color: #DD4A68; */
 }
-
 .nuxt-content ul {
   @apply ml-4 mb-6;
   @apply text-gray-500;
 }
 .nuxt-content li:before {
-  content: '•';
+  content: "•";
   font-family: sans-serif;
   @apply mr-4;
 }
